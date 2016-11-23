89c6287ffdad388eb80136cd1b2f4b2c2b29628b
pk <huienhuang@gmail.com>
2016-11-23 02:17:25 -0800

    server: add log config

--- server/lib/Logger.js
+++ server/lib/Logger.js
@@ -1,9 +1,11 @@
 var gCfg = require('../config');
 var gWinston = require('winston');
 
-var logger = module.exports = new gWinston.Logger({
-	transports: [
-		new gWinston.transports.Console(),
-		//new (winston.transports.File)({ filename: 'somefile.log' })
-	]
-});
+var transports = [
+	new gWinston.transports.Console()
+];
+if(gCfg.logFilename) {
+	transports.push(new (gWinston.transports.File)({ filename: gCfg.logFilename }));
+}
+
+var logger = module.exports = new gWinston.Logger({ transports: transports });
